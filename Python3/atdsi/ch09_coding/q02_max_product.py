3cd11f910f48e4866b718f16d7db7bc8d442e80b
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-28 15:05:09 +0200

    implement generic solution

--- Python3/atdsi/ch09_coding/q02_max_product.py
+++ Python3/atdsi/ch09_coding/q02_max_product.py
@@ -3,10 +3,17 @@ import heapq
 
 from typing import TYPE_CHECKING, Iterable, NewType, Sequence
 import math
+from itertools import islice
 
 # See: https://mypy.readthedocs.io/en/stable/runtime_troubles.html#using-types-defined-in-stubs-but-not-at-runtime
 if TYPE_CHECKING:
     from _typeshed import SupportsRichComparison
+
+
+# -----------------------------------------------------------------------------
+
+
+# The best solution is: `get_max_product_4`
 
 
 # -----------------------------------------------------------------------------
@@ -89,22 +96,49 @@ def _try_max_multiplying_head_with_last_2_negative_numbers(
         return max(prod_first_k(x, k), x[0] * math.prod(x[len(x) - 2 :]))
 
 
-def _leave_even_size_of_negative_numbers_only_mut(
-    x_sorted_desc: Sequence[SupportsRichComparison],
-) -> Sequence[SupportsRichComparison]:
-    if is_last_non_negative(x_sorted_desc):
+# Copied from: https://docs.python.org/3/library/itertools.html#itertools-recipes
+#
+# Replace by itertools.batched when Python 3.12 is released: https://docs.python.org/3.12/library/itertools.html#itertools.batched
+def batched(iterable, n):
+    "Batch data into tuples of length n. The last batch may be shorter."
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError("n must be at least one")
+    it = iter(iterable)
+    while batch := tuple(islice(it, n)):
+        yield batch
+
+
+def _get_max_product_4__find_max_mult(
+    largest_head: Sequence[SupportsRichComparison],
+    smallest_tail_with_maybe_negatives: Sequence[SupportsRichComparison],
+    k: NonNegativeInt,
+) -> SupportsRichComparison:
+    ret = prod_first_k(largest_head, k)
+
+    if is_last_non_negative(smallest_tail_with_maybe_negatives):
         # Optimization to quickly check if there are no negative numbers
-        return []
+        return ret
+
     else:
-        while len(x_sorted_desc) > 0:
-            if x_sorted_desc[0] >= 0:
-                del x_sorted_desc[0]
+        tail_prod: SupportsRichComparison = _PROD_START
+        tail_taken: int = 0
+
+        for negatives_pair in batched(reversed(smallest_tail_with_maybe_negatives), 2):
+            if len(negatives_pair) < 2 or not all(e < 0 for e in negatives_pair):
+                break
             else:
-                if len(x_sorted_desc) % 2 == 0:
-                    return x_sorted_desc
-                else:
-                    del x_sorted_desc[0]
-                    return x_sorted_desc
+                tail_taken += 2
+                tail_prod *= math.prod(negatives_pair)
+
+                total = tail_prod * prod_first_k(largest_head, k - tail_taken)
+
+                if total > ret:
+                    ret = total
+                elif ret > 0:
+                    break
+
+        return ret
 
 
 # -----------------------------------------------------------------------------
@@ -167,7 +201,15 @@ def get_max_product_3(x: Sequence[SupportsRichComparison], k: NonNegativeInt) ->
 
 def get_max_product_4(x: Iterable[SupportsRichComparison], k: NonNegativeInt) -> SupportsRichComparison:
     """
-    ?
+    Generic implementation, works with negative numbers and for any k
+
+    Complexity:
+    * Worst case if (k*2 > n):
+        * Time: O(n * log(n))
+        * Space: O(n)
+    * Else (normal case, k << n):
+        * Time: O(n * log(k))
+        * Space: O(k)
     """
     match k:
         case 0:
@@ -197,12 +239,7 @@ def get_max_product_4(x: Iterable[SupportsRichComparison], k: NonNegativeInt) ->
                 # Space O(k) -- worst case: Space(n/2)
                 smallest_tail = heapq.nsmallest(get_even_number_same_or_one_less(k), x_seq)
 
-            smallest_tail_abs = _leave_even_size_of_negative_numbers_only_mut(smallest_tail)
-
-            return max(
-                prod_first_k(largest_head, k),
-                math.prod(smallest_tail_abs) * prod_first_k(largest_head, k - len(smallest_tail_abs)),
-            )
+            return _get_max_product_4__find_max_mult(largest_head, smallest_tail, k)
 
 
 # -----------------------------------------------------------------------------
@@ -241,24 +278,3 @@ TEST_CASES_NEGATIVE_GENERIC = [
 def test_generic():
     run_test_cases(TEST_CASES_NEGATIVE_GENERIC, get_max_product_4)
     run_test_cases(TEST_CASES_NON_GENERIC, get_max_product_4)
-
-
-# -----------------------------------------------------------------------------
-
-
-def test_how_many_last_nums_are_negative():
-    TEST_CASES = [
-        (([], 0), 0),
-        (([], 1), 0),
-        (([3], 0), 0),
-        (([0], 0), 0),
-        (([0], 1), 0),
-        (([-1], 1), 1),
-        (([-1], 2), 1),
-        (([8, -9], 2), 1),
-        (([-8, -9], 2), 2),
-        (([-8, -9], 1), 1),
-        (([3, -8, -9], 3), 2),
-    ]
-
-    run_test_cases(TEST_CASES, how_many_last_k_nums_are_negative)
