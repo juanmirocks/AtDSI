c23017c536b325081de4bb537218c44800465cc4
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-08-13 14:53:44 +0200

    write new test util: gen_random_seq

--- Python3/atdsi/tutil.py
+++ Python3/atdsi/tutil.py
@@ -1,4 +1,12 @@
-from typing import Callable, Sequence, TypeVar
+from __future__ import annotations
+
+from typing import Callable, Sequence, TYPE_CHECKING, TypeVar
+
+if TYPE_CHECKING:
+    from _typeshed import SupportsLenAndGetItem
+
+import random
+import string
 
 
 Output = TypeVar("Output")
@@ -17,3 +25,8 @@ def run_test_cases(test_cases: Sequence[tuple[tuple, Output]], *funs: Callable[.
             assert equal(
                 expected_output, output
             ), f"\n\nin:\n{input_str}]\n\n->\n\nexpected_out:\n{expected_output}\n\n vs. \n\nout:\n{output}"
+
+
+def gen_random_seq(population: SupportsLenAndGetItem = string.ascii_lowercase, min: int = 1, max: int = 1):
+    k = min if min == max else random.randint(min, max)  # nosec B311
+    return random.choices(population, k=k)  # nosec B311
